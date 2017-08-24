195d61ddcf387636688361b8570c8688946c4d0b
Stévan Le Meur <slemeur@redhat.com>
2017-08-24 07:32:51 +0000

    This is a test commit, and pushing

--- src/main/java/io/openshift/booster/service/GreetingProperties.java
+++ src/main/java/io/openshift/booster/service/GreetingProperties.java
@@ -23,7 +23,7 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties("greeting")
 public class GreetingProperties {
 
-    private String message = "Hello, %s!";
+    private String message = "Bonjour, %s!";
 
     public String getMessage() {
         return message;
